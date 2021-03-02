27b227101ff72054c91926fb3c8430f4f8352d95
farhanayyash <farhanayyash16@yahoo.com>
2021-03-02 14:31:58 +0200

    lab13

--- server.js
+++ server.js
@@ -7,19 +7,27 @@ const app = express();
 const PORT = process.env.PORT
 const superagent = require('superagent');
 const pg = require('pg');
+const methodOverride = require('method-override');
+
 
 // const client = new pg.Client(process.env.DATABASE_URL);
 const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
 
 app.set('view engine', 'ejs');
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'))
+
+app.use(methodOverride('_method'));
 
 
 app.get('/getBook/:bookID', bookDetailsHandler)
 app.get('/searches/new', newBookHandler)
 app.post('/addBook', addBookHandler)
+app.put('/updateBook/:BookID', updateHandler);
+
+app.delete('/deleteBook/:BookID', deleteHandler)
 app.get('/', (req, res) => {
     let SQL = `SELECT * FROM books ORDER BY id DESC;`;
     client.query(SQL)
@@ -60,6 +68,37 @@ function Book(data) {
     this.description = data.volumeInfo.description || `There is no description`
 
 }
+
+function deleteHandler(req,res) {
+    let SQL = `DELETE FROM books WHERE id=$1;`;
+    let value = [req.params.BookID];
+    client.query(SQL,value)
+    .then(()=>{
+      res.redirect('/');
+    })
+  }
+  
+
+  function updateHandler(req, res) {
+    //collect data from form (req.body)
+    // update the data
+    // rediret to the same page
+    console.log(req.body);
+    // let title = req.body.title;
+    // let description = req.body.description;
+    let {author, title, isbn, description } = req.body;
+    console.log(title);
+    let SQL = `UPDATE books SET author=$1,title=$2,isbn=$3,description=$4 WHERE id =$5;`;
+    let values = [author, title, isbn, description,  req.params.BookID];
+    client.query(SQL, values)
+      .then(() => {
+        res.redirect(`/getBook/${req.params.BookID}`);
+      })
+      .catch(err => {
+        errorHandler(err, req, res)
+      })
+  
+  }
 
 function errorHandler(error, req, res) {
     res.render('pages/error', { err: error });
